6fedc2e0ec9f15ca5b677153d45167db27a08730
nicolangiano <nicolangiano@gmail.com>
2018-08-06 22:30:52 -0300

    Todo ok
    etapa 2 terminada - si lo veo al venezolano le lleno la cara de dedos, por imbécil

---
+++ wp-content/plugins/custom-upload/js/uploadPanel.js
@@ -0,0 +1,101 @@
+(function($){
+
+  let UploadController = function(){
+
+    let self = this;
+
+    let eventListener;
+
+    showPreloadFileList = function(){
+      var input = document.getElementById('uploadInput');
+      var output = $('.uc-file-name');
+      output.append(input.files.item(0).name);
+    }
+
+    self.init = function(listener){
+      eventListener = listener;
+    }
+
+    self.run = function(){
+
+      $(eventListener).on('change', '#uploadInput', function(){
+        console.log('asdasd');
+        showPreloadFileList();
+      });
+    }
+  }
+
+  let Navigator = function(){
+    let self = this;
+
+    let eventListener = '#fileTree';
+
+    let target = '#fileTree .list';
+
+    let actualDir;
+
+    let filename;
+
+    let navigate = function(dirName){
+      var data = {
+        'path': dirName,
+        'action': 'cu_navigate',
+      }
+      $.get(ajaxurl, data, function(data){
+        $(target).html(data);
+      })
+    }
+
+    self.run = function(){
+
+      $(eventListener).off().on('click', '.uc-dir', function(){
+        let dirName = $(this).text();
+        navigate(dirName);
+      });
+
+      $(eventListener).on('click', '#ucGoBack', function(){
+        navigate();
+      });
+
+    }
+  }
+
+
+  var loadUserContentCallback = function(form, action, target){
+    var data = {
+      'user': $(form).serialize(),
+      'action': action,
+    }
+    $.post(ajaxurl, data, function(data){
+      $(target).html(data);
+    })
+  }
+
+  $(document).ready(function(){
+
+    let root = '#customUploadPanel';
+
+    $(root).off().on('submit', '#filesByClientForm', function(e){
+      let loader = '<i class="fa fa-spinner fa-pulse fa-5x fa-fw" aria-hidden="true"></i>';
+      e.preventDefault(); e.stopPropagation();
+      $('#filesPermissionTable').html(loader);
+      loadUserContentCallback(this, 'load_permission', '#filesPermissionTable');
+
+    });
+
+    $(root).on('submit', '#downloadsByClientForm', function(e){
+      let loader = '<i class="fa fa-spinner fa-pulse fa-5x fa-fw" aria-hidden="true"></i>';
+      e.preventDefault(); e.stopPropagation();
+      $('#filesDownloadsTable').html(loader);
+      loadUserContentCallback(this, 'load_history', '#filesDownloadsTable');
+    });
+
+    let controller = new UploadController();
+    let nav = new Navigator();
+
+    controller.init(root);
+    controller.run();
+    nav.run();
+  })
+}
+)(jQuery);
---
+++ wp-content/themes/Avada/time.php
@@ -0,0 +1,7 @@
+<?php
+echo "ajjdjahuwhwihhiooiiotiotoitalgo";
+$History = new History;
+$date = date('Y-m-d H:i:s');
+$params = array('id'=>, 'user_id'=> $user->ID, $value['file_id'], 'date'=> $date);
+$History->add($params);
+?>
