e92807700f4152e1e6cf1793e06894f796d0e4f1
nicolangiano <nicolangiano@gmail.com>
2018-10-26 12:05:20 -0300

    actualizacion solapa de coordinar clientes, solo muestra

--- wp-content/plugins/globalsax-core/pantallaAdminGlobalsax.php
+++ wp-content/plugins/globalsax-core/pantallaAdminGlobalsax.php
@@ -3,18 +3,43 @@ define('GLOBALSAX_CORE','globalsax-core');
 
 function theme_settings_page()
 {
+  $screen =  get_current_screen();
+	$pluginPageUID = $screen->parent_file;
     ?>
-	    <div class="wrap">
-	    <h1>GLOBALSAX - CORE</h1>
-	    <form method="post" action="options.php">
-	        <?php
-	            settings_fields("section");
-	            do_settings_sections("theme-options");
-	            submit_button();
-	        ?>
-	    </form>
+    <div class="wrap">
+        <h1 class="panel-title">GLOBALSAX - CORE</h1>
+          <h2 class="nav-tab-wrapper">
+          <a href="<?= admin_url('admin.php?page='.$pluginPageUID.'&tab=sincronizar')?>" class="nav-tab">Sincronizar</a>
+          <a href="<?= admin_url('admin.php?page='.$pluginPageUID.'&tab=assignClient')?>" class="nav-tab">Asignar clientes</a>
+        </h2>
+
+      <div class="panel-body">
+  			<?php $activeTab = $_GET['tab']; ?>
+
+  			<?php if (!isset($activeTab)){ ?>
+        	<div id="gs-tab"><?php settings(); ?></div>
+  			<?php } ?>
+
+  			<?php if ($activeTab == 'sincronizar'){ ?>
+  				<div class="gs-tab"><?php	settings(); ?></div>
+  			<?php } ?>
+
+        <?php if ($activeTab == 'assignClient'){ ?>
+  				<div class="gs-tab"><?php	assignClient(); ?></div>
+  			<?php } ?>
 		</div>
 	<?php
+}
+function settings(){
+  ?>
+  <form method="post" action="options.php">
+   <?php
+       settings_fields("section");
+       do_settings_sections("theme-options");
+       submit_button();
+   ?>
+</form>
+<?php
 }
 function display_opcion_sincronizar_productos()
 {
@@ -86,4 +111,40 @@ function display_theme_panel_fields()
 }
 
 add_action("admin_init", "display_theme_panel_fields");
+
+
+function get_GS_user(){
+  global $wpdb;
+  $gs_client_table = $wpdb->prefix . ('gs_clients');
+  $query = 'SELECT * FROM ' . $gs_client_table . ' WHERE NOT ISNULL(user_id) ';
+  return $wpdb->get_results($query);
+}
+
+function assignClient(){
+?>
+  <table>
+      <tr>
+        <th>Cliente</th>
+        <th>Usuario Wordpress</th>
+        <th>Borrar relación</th>
+      </tr>
+      <tr>
+        <?php
+        $clientes = get_GS_user();
+        foreach ($clientes as $key => $cliente) {
+          $userByID = get_user_by('ID', $cliente->user_id);
+          ?>
+        <td><?php echo $cliente->Name; ?></td>
+        <td><?php echo $userByID->display_name; ?></td>
+        <td><?php echo '$userByID'; ?></td>
+      </tr>
+    <?php } ?>
+  </table>
+    <div class="cu-submit-button cu-text-center">
+      <button type="submit">Editar características</button>
+      <div class="cu-loader"></div>
+    </div>
+<?php
+}
+
 ?>
